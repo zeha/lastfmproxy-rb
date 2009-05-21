86495e02b598e6b883d4d64939f0ae369b8509a6
Christian Hofstaedtler <ch+git@zeha.at>
2009-05-21 22:38:12 +0200

    Add instructions, and a license.

--- proxy.rb
+++ proxy.rb
@@ -1,12 +1,47 @@
 #!/usr/bin/ruby
-
+#
+# lastfmproxy-rb
+#
+# An HTTP stream proxy for last.fm radio.
+#
+# You must be a last.fm subscriber and must get an API Key from last.fm, or
+# this will not work. See http://last.fm/api for the API Key application.
+#
+# Clients currently tested: SqueezeBox.
+#
+# ----------------------------------------------------------------------------
+# Copyright (c) 2009 Christian Hofstaedtler <ch+lastfmproxy@zeha.at>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ----------------------------------------------------------------------------
+#
+# Configuration:
+# Copy the following section to ./config.rb and edit it there.
 username = 'USERNAME'
 password = 'PASSWORD'
 station = 'lastfm://user/'+username+'/personal'
 api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 api_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-
+# ----------------------------------------------------------------------------
+#
 # don't need to change anything below
+#
 
 if File.exists?('./config.rb')
   eval(File.read('./config.rb'))
